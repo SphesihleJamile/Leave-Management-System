ffdbb22b4f510f07fa8fd06a85aeafec53bfcc89
Sphesihle Jamile <jamilesphesihle99@gmail.com>
2023-03-03 16:01:45 +0200

    Added Database Tables and Migrations, End Implement Database Section

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string? FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
+        public string? TaxId { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
